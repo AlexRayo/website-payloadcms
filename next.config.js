26070562b0529801cba796350e0ca32a3c66e6ef
alexrayo <yutarunet@gmail.com>
2025-10-08 16:26:05 -0600

    first commit

--- next.config.js
+++ next.config.js
@@ -10,12 +10,14 @@ const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
 const nextConfig = {
   images: {
     remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
+      ...[NEXT_PUBLIC_SERVER_URL, 'http://192.168.40.11' /* 'https://example.com' */].map((item) => {
         const url = new URL(item)
 
         return {
           hostname: url.hostname,
           protocol: url.protocol.replace(':', ''),
+          port: url.port || undefined,
+          pathname: '/api/media/file/**',
         }
       }),
     ],
